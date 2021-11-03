8deba01d47c463bd6807261923a941a9ac5cc556
Anthony-S58 <tonysim01@hotmail.com>
2021-11-03 08:50:15 +0100

    AddRecupActus

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration;
 use App\Entity\Post;
+use App\Entity\Actualites;
+
 use Doctrine\DBAL\Driver\SQLSrv\LastInsertId;
 
 class IndexController extends AbstractController
@@ -20,13 +22,16 @@ class IndexController extends AbstractController
         $postcol1 = $this->getDoctrine()->getRepository(Post::class)->findBy([], ['id' => 'DESC'], 2, 0);
         $postcol2 = $this->getDoctrine()->getRepository(Post::class)->findBy([], ['id' => 'DESC'], 2, 2);
         $postcol3 = $this->getDoctrine()->getRepository(Post::class)->findBy([], ['id' => 'DESC'], 2, 4);
+        $actus = $this->getDoctrine()->getRepository(Actualites::class)->findBy([], ['id' => 'DESC'], 2);
+
 
 
         return $this->render('index/index.html.twig', [
             'user' => $user,
             'postcol1' => $postcol1,
             'postcol2' => $postcol2,
-            'postcol3' => $postcol3
+            'postcol3' => $postcol3,
+            'actus' => $actus
 
 
         ]);
