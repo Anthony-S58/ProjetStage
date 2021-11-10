b7a34f73674203f2a0531f808cad2b3198b2a851
Anthony-S58 <tonysim01@hotmail.com>
2021-11-10 12:02:39 +0100

    AddFilters

--- src/Controller/ActualitesController.php
+++ src/Controller/ActualitesController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Actualites;
+use App\Entity\Chocolaterie;
+
 
 class ActualitesController extends AbstractController
 {
@@ -17,10 +19,32 @@ class ActualitesController extends AbstractController
         $user = $this->getUser();
 
         $actus = $this->getDoctrine()->getRepository(Actualites::class)->findBy([], ['id' => 'DESC']);
+        $chocolateries = $this->getDoctrine()->getRepository(Chocolaterie::class)->findBy([], ['ville' => 'ASC']);
+
 
         return $this->render('actualites/index.html.twig', [
             'user' => $user,
-            'actus' => $actus
+            'actus' => $actus,
+            'chocolateries' => $chocolateries
+
+        ]);
+    }
+
+    /**
+     * @Route("/actualites/{id}", name="actu")
+     */
+    public function actu($id): Response
+    {
+        $user = $this->getUser();
+
+        $actus = $this->getDoctrine()->getRepository(Actualites::class)->findBy(['chocolaterie' => $id], ['id' => 'DESC']);
+        $chocolateries = $this->getDoctrine()->getRepository(Chocolaterie::class)->findBy([], ['ville' => 'ASC']);
+
+
+        return $this->render('actualites/index.html.twig', [
+            'user' => $user,
+            'actus' => $actus,
+            'chocolateries' => $chocolateries
         ]);
     }
 }
--- src/Controller/ViedeschocolateriesController.php
+++ src/Controller/ViedeschocolateriesController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Post;
 use App\Entity\User;
+use App\Entity\Categories;
 
 
 class ViedeschocolateriesController extends AbstractController
@@ -17,13 +18,36 @@ class ViedeschocolateriesController extends AbstractController
     public function index(): Response
     {
         $user = $this->getUser();
-        $users = $this->getDoctrine()->getRepository(User::class)->findBy([], ['id' => 'DESC'], 5);
+        $users = $this->getDoctrine()->getRepository(User::class)->findBy([], ['id' => 'DESC'],5);
         $posts = $this->getDoctrine()->getRepository(Post::class)->findBy([], ['id' => 'DESC']);
+        $categories = $this->getDoctrine()->getRepository(Categories::class)->findBy([], ['id' => 'ASC']);
+
 
         return $this->render('viedeschocolateries/index.html.twig', [
             'user' => $user,
             'users' => $users,
-            'posts' => $posts
+            'posts' => $posts,
+            'categories' => $categories
+
+        ]);
+    }
+
+    /**
+     * @Route("/vie-des-chocolateries/{id}", name="viechoco")
+     */
+    public function postcateg($id): Response
+    {
+        $user = $this->getUser();
+        $users = $this->getDoctrine()->getRepository(User::class)->findBy([], ['id' => 'DESC'],5);
+        $posts = $this->getDoctrine()->getRepository(Post::class)->findBy(['categories' => $id], ['id' => 'DESC']);
+        $categories = $this->getDoctrine()->getRepository(Categories::class)->findBy([], ['id' => 'ASC']);
+
+
+        return $this->render('viedeschocolateries/index.html.twig', [
+            'user' => $user,
+            'users' => $users,
+            'posts' => $posts,
+            'categories' => $categories
         ]);
     }
 }
