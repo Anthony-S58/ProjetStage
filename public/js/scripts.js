5b2d8803740b85aa2e08f9401d895e58288f5b0b
Anthony-S58 <tonysim01@hotmail.com>
2021-11-18 09:17:12 +0100

    AddAnimationAndAddPostCss

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,9 +5,14 @@ let head = document.querySelector('header');
 
 close.addEventListener('click',() => {
     head.style.display= 'none';
+    head.classList.remove("open");
+    head.classList.add("close");
 });
 
 open.addEventListener('click',() => {
     head.style.display= 'flex';
+    head.classList.remove("close");
+    head.classList.add("open");
+
 });
 
